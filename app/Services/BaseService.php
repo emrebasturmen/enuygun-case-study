7915f261e9edff6fc3bd2a2dfee1b7541c4a1230
Emre <ebasturmen@outlook.com>
2022-08-11 14:05:50 +0300

    BaseService added. DeveloperService added and updated for store data.

---
+++ app/Services/BaseService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class BaseService
+{
+    /**
+     * TODO: Can be implement here response functions like API Response.
+     */
+    function __construct()
+    {
+        // In this project I not change the any response body to save time.
+    }
+}
