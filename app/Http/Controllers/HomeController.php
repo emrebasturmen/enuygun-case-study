9c2f024c84ef4e0fad954ee564b1cda0313d52c2
Emre <ebasturmen@outlook.com>
2022-08-11 15:06:33 +0300

    HomeController updated for getting task schedule.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\Developer;
+use App\Models\Task;
+use App\Schedule\TaskSchedule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
+    /**
+     * @var TaskSchedule
+     */
+    private TaskSchedule $taskSchedule;
+
+    /**
+     * Constructor.
+     *
+     * @param TaskSchedule $taskSchedule
+     */
+    public function __construct(TaskSchedule $taskSchedule)
+    {
+        $this->taskSchedule = $taskSchedule;
+    }
+
+    /**
+     * @return void
+     */
     public function index()
     {
-        //
+        $developers = Developer::orderBy('hourly_capacity','ASC')->get()->toArray();
+        $tasks = Task::select('name',DB::raw('duration * difficulty as duration'), DB::raw('1 as difficulty'))->orderBy('duration','ASC')->get()->toArray();
+        $planning = $this->taskSchedule->schedule($tasks, $developers);
+
+        dd($planning);
     }
 }
