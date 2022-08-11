f5d83a41cd57d27c3cba337ceb8e2f34ceb1987e
Emre <ebasturmen@outlook.com>
2022-08-11 13:41:48 +0300

    Task and developer model files added.

---
+++ app/Models/Developer.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Developer extends Model
+{
+    use HasFactory;
+
+    /**
+     * Guarded columns.
+     *
+     * @var string[]
+     */
+    protected $guarded = [
+        'id'
+    ];
+
+    /**
+     * Fillable columns.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'hourly_capacity',
+        'weekly_capacity'
+    ];
+
+    /**
+     * @param $value
+     * @return void
+     */
+    public function setNameAttribute($value): void
+    {
+        $this->attributes['name'] = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNameAttribute(): mixed
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param $value
+     * @return void
+     */
+    public function setHourlyCapacityAttribute($value): void
+    {
+        $this->attributes['hourly_capacity'] = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHourlyCapacityAttribute(): mixed
+    {
+        return $this->hourly_capacity;
+    }
+
+    /**
+     * @param $value
+     * @return void
+     */
+    public function setWeeklyCapacityAttribute($value): void
+    {
+        $this->attributes['weekly_capacity'] = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWeeklyCapacityAttribute(): mixed
+    {
+        return $this->weekly_capacity;
+    }
+}
---
+++ app/Models/Task.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    /**
+     * Guarded columns.
+     *
+     * @var string[]
+     */
+    protected $guarded = [
+        'id'
+    ];
+
+    /**
+     * Fillable columns.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'difficulty',
+        'duration'
+    ];
+
+    /**
+     * @param $value
+     * @return void
+     */
+    public function setNameAttribute($value): void
+    {
+        $this->attributes['name'] = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNameAttribute(): mixed
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param $value
+     * @return void
+     */
+    public function setDifficultyAttribute($value): void
+    {
+        $this->attributes['difficulty'] = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDifficultyAttribute(): mixed
+    {
+        return $this->difficulty;
+    }
+
+    /**
+     * @param $value
+     * @return void
+     */
+    public function setDurationAttribute($value): void
+    {
+        $this->attributes['duration'] = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDurationAttribute(): mixed
+    {
+        return $this->duration;
+    }
+}
